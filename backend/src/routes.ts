96dc373990bf1ac865768eeb82a1ee4fea9b3cf4
AlbertoDamaso <alberto.matheus@hotmail.com>
2023-01-05 15:54:10 -0300

    rabitNewErrorTratativ

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,7 +3,8 @@ import { Router,Request, Response } from "express";
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-    return res.json({ nome: 'Rabit New' })
+    return res.json({ status: 'Ativo' })
+    // throw new Error('Erro ao fazer essa requisição.')
 })
 
 export { router };
