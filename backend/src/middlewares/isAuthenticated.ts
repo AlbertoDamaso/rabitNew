45d6bedd5f762b4fbe0afd24654017bdbdb08f10
AlbertoDamaso <alberto.matheus@hotmail.com>
2023-01-09 16:41:58 -0300

    rabitNewMiddleware

---
+++ backend/src/middlewares/isAuthenticated.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface Payload{
+    sub: string;
+}
+
+export function isAuthenticated(
+    req: Request,
+    res: Response,
+    next: NextFunction
+){
+    // Receber token
+    const authToken = req.headers.authorization;
+
+    if(!authToken){
+        return res.status(401).end();
+    }
+
+    const [, token] = authToken.split(" ")
+
+    try{
+        // Validar esse token
+        const { sub } = verify(
+            token,
+            process.env.JWT_SECRET
+        ) as Payload;
+        
+        // Recuperar o id do token e colocar dentro de uma variavel user_id dentro do req.
+        req.user_id = sub;
+
+        return next();
+
+    }catch(err){
+        return res.status(401).end();
+    }
+}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
 //ROTAS USER
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
+router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 export { router };
