36c7d56c18d1ef4ffcc6932c20bb6e6b17527b92
AlbertoDamaso <alberto.matheus@hotmail.com>
2023-01-11 19:10:27 -0300

    rabitNewLoginScss

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,9 @@
 import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/home.module.scss"
+
+import logoImg from "../../public/logo.svg"
+import { Input } from "../components/ui/Input";
 
 export default function Home() {
   return (
@@ -7,9 +12,19 @@ export default function Home() {
       <title>RabitNew - Faça seu login</title>
     </Head>
     <div>
-      <h1>
-        Rabit New
-      </h1>
+      <Image src={logoImg} alt="Logo Rabit New"/>
+      <div className={styles.login}>
+        <form>
+          <Input
+            placeholder="E-mail"
+            type="text"
+          />
+          <Input
+            placeholder="Password"
+            type="password"
+          />
+        </form>
+      </div>
     </div>
     </>
   )
