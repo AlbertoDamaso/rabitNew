c14309a0796b72ccc9be3b6c016c53dedd6ba822
AlbertoDamaso <alberto.matheus@hotmail.com>
2023-01-16 12:32:11 -0300

    rabitNewAuthContext

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useContext } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
@@ -6,8 +7,23 @@ import styles from "../styles/home.module.scss"
 import logoImg from "../public/logo.svg"
 import { Input } from "../components/ui/Input";
 import { Button } from "../components/ui/Button";
+import { AuthContext } from "../contexts/AuthContext";
 
 export default function Home() {
+
+  const { signIn } = useContext(AuthContext)
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data={
+      email: "alberto@teste.com",
+      password: "213213"
+    }
+
+    await signIn(data)
+  }
+
   return (
     <>
     <Head>
@@ -16,7 +32,7 @@ export default function Home() {
     <div className={styles.containerCenter}>
       <Image src={logoImg} alt="Logo Rabit New"/>
       <div className={styles.login}>
-        <form>
+        <form onSubmit={handleLogin}>
           <Input
             placeholder="E-mail"
             type="text"
