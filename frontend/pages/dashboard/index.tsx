6b5ea92b7d9b4994bd163df82d3231d8eaf5288f
AlbertoDamaso <alberto.matheus@hotmail.com>
2023-01-17 21:43:54 -0300

    rabitNewOrder

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -1,8 +1,30 @@
-import Head from "next/head"
-import { Header } from "../../components/Header"
-import { canSSRAuth } from "../../utils/canSSRAuth"
+import { useState } from "react";
+import Head from "next/head";
+import { FiRefreshCcw } from "react-icons/fi";
+import { Header } from "../../components/Header";
+import { setupAPIClient } from "../../services/api";
+import { canSSRAuth } from "../../utils/canSSRAuth";
+import styles from "./styles.module.scss";
 
-export default function Dashboard(){
+type OrderProps = {
+    id: string;
+    table: string | number;
+    status: boolean;
+    draft: boolean;
+    name: string | null;
+}
+interface HomeProps{
+    order: OrderProps[];
+}
+
+export default function Dashboard({ order }: HomeProps){ 
+
+    const [orderList, setOrderList] = useState(order  || []);
+
+    function handleOpenModalView(id: string){
+        alert("test" + id)
+    }
+
     return(
         <>
         <Head>
@@ -10,6 +32,27 @@ export default function Dashboard(){
         </Head>
         <div>
             <Header/>
+
+            <main className={styles.container}>
+                <div className={styles.containerHeader}>
+                    <h1>Últimos pedidos</h1>
+                    <button>
+                        <FiRefreshCcw size={25} color="#3fffa3"/>
+                    </button>
+                </div>
+
+                <article className={styles.listOrders}>
+
+                    {orderList.map( item => (
+                        <section key={item.id} className={styles.orderItem}>
+                            <button onClick={ () => handleOpenModalView(item.id) }>
+                                <div className={styles.tag}></div>
+                                <span>Mesa {item.table}</span>
+                            </button>
+                        </section>
+                    ))}
+                </article>
+            </main>
         </div>
         </>
     )
@@ -17,7 +60,15 @@ export default function Dashboard(){
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
     
+    const apiClient = setupAPIClient(ctx);
+
+    const response = await apiClient.get('/order');
+
+    // console.log(response.data)
+
     return{
-        props:{}
+        props:{
+            order: response.data
+        }
     }
 })
