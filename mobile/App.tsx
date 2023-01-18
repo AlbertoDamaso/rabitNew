f8513f04abd7476de707786b30787542bdee51d2
AlbertoDamaso <alberto.matheus21@gmail.com>
2023-01-18 14:36:24 -0300

    rabitNewStartMobile

---
+++ mobile/App.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar backgroundColor='#006442' barStyle='light-content' translucent={false}/>
+    </View>
+  )
+}
