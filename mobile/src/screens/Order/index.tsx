00cd537a7b5898f4762d8bf2b70524e606d63b0c
AlbertoDamaso <alberto.matheus21@gmail.com>
2023-01-19 18:03:28 -0300

    rabitNewOrder

--- mobile/src/screens/Order/index.tsx
+++ mobile/src/screens/Order/index.tsx
@@ -1,18 +1,31 @@
+import { useRoute, RouteProp } from '@react-navigation/native';
 import React from 'react';
-
 import {
   SafeAreaView,
   Text
 } from 'react-native';
 import { Background } from '../../components/Background';
-
 import { styles } from './styles';
 
+type RouteDetailParams = {
+  Order:{
+    number: string | number;
+    order_id: string;
+  }
+}
+
+type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>;
+
 export function Order() {
+
+  const route = useRoute<OrderRouteProps>();
+
   return (
     <Background>
         <SafeAreaView style={styles.container}>
-            <Text>Tela order</Text>
+            <Text>
+              {route.params.number}
+            </Text>
         </SafeAreaView>
     </Background>
   );
